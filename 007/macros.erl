7bbbdbb31acba31a757ebd40e80f61f5261faa8f
Alexey Shilin <shilin.alexey@gmail.com>
2019-01-30 08:01:54 +0300

    Add: 7-7 macros - count the number of function calls

--- 007/macros.erl
+++ 007/macros.erl
@@ -1,5 +1,6 @@
 -module(macros).
 -export([test/0]).
+-export([some_func/1]).
 %-include("macros.hrl").
 
 -define(
@@ -11,9 +12,25 @@
 	end
 ).
 
+-define(debug, ok).
+
+-ifdef(debug).
+	-define(CALLS(Func), io:format("[call ~p]~n", [??Func])).
+-else.
+	-define(CALLS(Func), ok).
+-endif.
+
+
+some_func(Param)->
+	?CALLS(some_func(Param)),
+	io:format("[test]"),
+	ok.
+
 test()->
 	?SHOW_EVAL(length([1,2,3]), show),
 	?SHOW_EVAL(length([1,2,3]), none),
+	some_func(111),
+	some_func(222),
 	ok.
 
 % c(macros).
