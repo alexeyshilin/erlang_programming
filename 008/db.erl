8767e259ee71fc41052aa442227d108bb8d40463
Alexey Shilin <shilin.alexey@gmail.com>
2019-02-03 22:59:32 +0300

    Add: ch8 db v. 1.0

---
+++ 008/db.erl
@@ -0,0 +1,32 @@
+-module(db).
+-export([new/0,write/3,read/2, delete/2,destroy/1]).
+-vsn(1.0).
+
+new()
+	-> dict:new().
+
+write(Key, Data, Db) -> dict:store(Key, Data, Db).
+
+read(Key, Db) ->
+	case dict:fetch(Key, Db) of
+		error
+			-> {error, instance};
+		{ok, Data} -> {ok, Data}
+	end.
+
+delete(Key, Db) -> diet:erase(Key, Db).
+
+destroy(Db)
+	-> ok.
+
+% c(db).
+%
+% Db = db:new().
+% Dbl = db:write(francesco, sanfrancisco, Db).
+% Db2 = db:write(alison, london, Dbl).
+% db:read(francesco, Db2).
+%
+%%** exception error: no case clause matching sanfrancisco
+%%     in function  db:read/2 (db.erl, line 11)
+%
+% dict:fetch(francesco, Db2).
