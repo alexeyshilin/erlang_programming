90bbc0d2eb92d771ef357174b26e169600f52194
Alexey Shilin <shilin.alexey@gmail.com>
2019-02-11 09:19:07 +0300

    Add: 9-1 funcs - print v2

--- 009/funcs.erl
+++ 009/funcs.erl
@@ -2,6 +2,16 @@
 -export([test/0]).
 -vsv(0.1).
 
+func_map(S, E, F)->
+	Res = lists:seq(S, E),
+	func_map(Res, F).
+
+func_map([H|[]], F)->
+	F(H);
+
+func_map([H|T], F)->
+	F(H),
+	func_map(T, F).
 
 test()->
 
@@ -16,6 +26,15 @@ end,
 
 
 	Fun1(1, 10, Fun1),
+
+	%Res = lists:seq(1, 10),
+	%io:format("[~p]", [Res]),
+
+FPrint = fun(V)->
+	io:format("[~p]", [V])
+end,
+
+	func_map(1,10, FPrint),
 
 	ok.
 
