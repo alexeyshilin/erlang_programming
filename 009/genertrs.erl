3814e4e5d7fa6f28b8d89e2485fb7d0559cc6ed1
Alexey Shilin <shilin.alexey@gmail.com>
2019-02-13 19:32:20 +0300

    Add: 9-2 gen filter

--- 009/genertrs.erl
+++ 009/genertrs.erl
@@ -3,8 +3,12 @@
 -vsv(0.1).
 
 
+
 test()->
 	[3,6,9] = [X || X <- lists:seq(1,10), X rem 3 == 0],
+
+	[1, 100, 9] = [ X || X <- [1, hello, 100, boo, "boo", 9], is_integer(X)],
+
 	ok.
 
 % c(genertrs).
