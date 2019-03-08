b5554bf6ef8cabe4fb61907b6427d55ebea9e253
Alexey Shilin <shilin.alexey@gmail.com>
2019-03-08 23:10:12 +0300

    Add: 12.3 supervisor

---
+++ 012/usr_sup.erl
@@ -0,0 +1,23 @@
+-module(usr_sup).
+-behavior(supervisor).
+%-behaviour(supervisor).
+-export([start_link/0]).
+-export([init/1]).
+
+start_link()->
+	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init(FileName) ->
+	UsrChild = { usr, { usr, start_link, [] }, permanent, 2000, worker, [usr, usr_db] } ,
+	{ok , {{ one_for_all, 1, 1}, [UsrChild]}}.
+
+% c(usr_sup).
+%
+% usr_sup:start(). % err
+% usr_sup:start_link().
+% whereis(usr).
+% exit(whereis(usr), kill).
+% whereis(usr).
+% usr:lookup_id(0).
+% exit(whereis(usr), kill).
+% exit(whereis(usr), kill).
