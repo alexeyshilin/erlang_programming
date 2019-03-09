cf00e9376cc0c33ca94e769da4f92188dff0907e
Alexey Shilin <shilin.alexey@gmail.com>
2019-03-09 12:49:25 +0300

    Chg: 12-2 superviser - init data

--- 012/my_db_sup.erl
+++ 012/my_db_sup.erl
@@ -5,10 +5,11 @@
 
 
 start_link() ->
-	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+	Data = [{test, test_val}],
+	supervisor:start_link({local, ?MODULE}, ?MODULE, Data).
 
 init(Data) ->
-	Chld = { my_db_gen, {my_db_gen, start, []}, permanent, 30*1000, worker, [my_db_gen] } ,
+	Chld = { my_db_gen, {my_db_gen, start, [Data]}, permanent, 30*1000, worker, [my_db_gen] } ,
 	{ok, {{one_for_all, 5, 60*60}, [Chld]}}.
 	%Response:
 	% {ok, {SupervisorSpecification, ChildSpecificationList}}
@@ -23,6 +24,7 @@ init(Data) ->
 % c(my_db_sup).
 % my_db_sup:start_link().
 % whereis(my_db_gen).
+% my_db_gen:read(test).
 % exit(whereis(my_db_gen), kill).
 % whereis(my_db_gen).
 % my_db_gen:write(foo, bar).
