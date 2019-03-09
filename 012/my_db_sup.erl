4364d6d9d7d8404a85464c8e2a64057a20e95069
Alexey Shilin <shilin.alexey@gmail.com>
2019-03-09 12:22:13 +0300

    Add: 12.7 ex 12-2 db server supervisor

---
+++ 012/my_db_sup.erl
@@ -0,0 +1,31 @@
+-module(my_db_sup).
+-behavior(supervisor).
+-export([start_link/0]).
+-export([init/1]).
+
+
+start_link() ->
+	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init(Data) ->
+	Chld = { my_db_gen, {my_db_gen, start, []}, permanent, 2000, worker, [my_db_gen] } ,
+	{ok, {{one_for_all, 1, 1}, [Chld]}}.
+	%Response:
+	% {ok, {SupervisorSpecification, ChildSpecificationList}}
+		% SupervisorSpecification = {RestartStrategy, AllowedRestarts, MaxSeconds}
+			% RestartStrategy = one_for_one, one_for_all, rest_for_all
+		% ChildSpecificationList = {Id, {Module, Function, Arguments}, Restart, Shutdown, Type, ModuleList}
+				% Restart = transient, temporary, permanent
+				% Shutdown = brutal kill ... 1 ... infinity
+				% Type = worker, supervisor
+
+
+% c(my_db_sup).
+% my_db_sup:start_link().
+% whereis(my_db_gen).
+% exit(whereis(my_db_gen), kill).
+% whereis(my_db_gen).
+% my_db_gen:write(foo, bar).
+% my_db_gen:read(foo).
+% exit(whereis(my_db_gen), kill).
+% exit(whereis(my_db_gen), kill).
