42fb4d8f3e01941f5baafa8c72b2e307bf78b95a
Alexey Shilin <shilin.alexey@gmail.com>
2019-03-20 18:40:11 +0300

    Add: datetime 2 unixstamp

--- 014/blog_db.erl
+++ 014/blog_db.erl
@@ -1,6 +1,6 @@
 -module(blog_db).
 -include("blog.hrl").
--export([unixtime/0, unixtime/1, unixtime2timestamp/1, timestamp2datetime/1, timestamp/0, get_timestamp/0, micro2timestamp/1, micro2timestamp/1]).
+-export([unixtime/0, unixtime/1, unixtime2timestamp/1, timestamp2datetime/1, timestamp/0, get_timestamp/0, micro2timestamp/1, micro2timestamp/1, datetime2unixstamp/1]).
 -export([create/1, open/1, close/1]).
 -export([add_item/2, add_item/3, delete_item/2, lookup_item/2, update_item/2, update_item/3]).
 -export([lookup_free/1, get_all/1, get_simple/1]).
@@ -40,9 +40,11 @@ unixtime2timestamp(Value)->
 gen_id()->
 	get_timestamp().
 
-%2000-01-01 00:00:00
-%946684800
-
+datetime2unixstamp( {{Y,M,D},{Hr,Min,Sec}} = DT )->
+	UnixSec = calendar:datetime_to_gregorian_seconds({{1970,01,01},{00,00,00}}), % 62167219200
+	UnixSec = 62167219200,
+	DtSec = calendar:datetime_to_gregorian_seconds(DT),
+	DtSec - UnixSec.
 
 create(Filename)->
 	dets:open_file(tmp, [{file, Filename}, {keypos, #post.id}]), % {ok, Name} | {error, Reason}
