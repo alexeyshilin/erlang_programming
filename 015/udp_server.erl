e93e69b0dc61f3c22f81024feda63c8079c98de5
Alexey Shilin <shilin.alexey@gmail.com>
2019-03-25 13:00:47 +0300

    Add: 15.1 udp example binary - passive mode

--- 015/udp_server.erl
+++ 015/udp_server.erl
@@ -4,10 +4,10 @@
 test()->
 	ok.
 
-% {ok, Socket} = gen_udp:open(1234,[binary,{header,2}]).
+% {ok, Socket} = gen_udp:open(1234,[{active, false}, binary,{header,2}]).
+
+% gen_udp:recv(Socket, 100, 1000*60).
 
 % % wait sendin packets from #2
-
-% flush().
 
 % gen_udp:close(Socket).
