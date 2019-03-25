d82a46ee15a37d43cf454cfaea26955cd06ff18e
Alexey Shilin <shilin.alexey@gmail.com>
2019-03-25 13:07:10 +0300

    Add: 15.3 inet example

--- 015/udp_server.erl
+++ 015/udp_server.erl
@@ -5,9 +5,12 @@ test()->
 	ok.
 
 % {ok, Socket} = gen_udp:open(1234,[{active, false}, binary,{header,2}]).
+% inet:getopts(Socket, [active, exit_on_close, header, nodelay]).
 
 % gen_udp:recv(Socket, 100, 1000*60).
 
 % % wait sendin packets from #2
+
+% inet:getstat(Socket).
 
 % gen_udp:close(Socket).
