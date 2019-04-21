57f70078e6c9481c68fc1b14d2e6ee636e4d729d
Alexey Shilin <shilin.alexey@gmail.com>
2019-04-21 11:33:14 +0300

    Add: 19-4 supervisor (12.3) [test]

--- 019/otp/gen_server/usr_sup.erl
+++ 019/otp/gen_server/usr_sup.erl
@@ -3,12 +3,14 @@
 -export([start/0]).
 -export([init/1]).
 
+-include_lib("eunit/include/eunit.hrl").
+
 start()->
 	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init(FileName) ->
 	UsrChild = {usr, {usr, start_link, []}, permanent, 2000, worker, [usr, usr_db]},
-	{ok, { {one_for_all, 1, 1 }, [UsrChild]}}.
+	{ok, { {one_for_all, 10, 1 }, [UsrChild]}}.
 
 % c(usr_sup).
 %
@@ -20,3 +22,60 @@ init(FileName) ->
 % exit(whereis(usr), kill).
 % exit(whereis(usr), kill).
 
+some_test_()->
+	{spawn,
+		{setup
+			,fun() -> ok end % init
+			,fun(_) -> ok end % clear
+
+			%test
+			, {inorder,
+				[
+					some_test1()
+					,some_test2()
+					,some_test3()
+				]
+			}
+		}
+	}.
+
+some_test1()->
+	Value1 = true,
+	Value2 = true,
+	[
+		?_assertEqual(true, true)
+		,?_assertEqual(Value1, Value2)
+	].
+
+some_test2()->
+	Value1 = true,
+	Value2 = true,
+	[
+		?_assertEqual(true, true)
+		,?_assertEqual(Value1, Value2)
+	].
+
+some_test3()->
+	Value1 = true,
+	Value2 = true,
+	[
+		?_assertEqual(true, true)
+		,?_assertEqual(Value1, Value2)
+
+		,?_assertMatch({ok,Pid} when is_pid(Pid), usr_sup:start())
+		,?_assertMatch(Pid when is_pid(Pid), whereis(usr))
+		,?_assertEqual(true, exit(whereis(usr), kill))
+		,?_assertMatch(Pid when is_pid(Pid), whereis(usr))
+
+		,?_assertEqual(ok, usr:add_usr(700000003, 3, prepay))
+		,?_assertEqual({ok,{usr,700000003,3,enabled,prepay,[]}}, usr:lookup_id(3))
+		
+		,?_assertEqual(true, exit(whereis(usr), kill))
+		,?_assertEqual(true, exit(whereis(usr), kill))
+
+		,?_assertMatch(Pid when is_pid(Pid), whereis(usr))
+	].
+
+% c(usr_sup).
+% usr_sup:test().
+% eunit:test(usr_sup).
